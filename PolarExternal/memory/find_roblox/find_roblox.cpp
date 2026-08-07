ffe25779d8ebbcf91f85e681228afcf9d7f5edfe
lakeviewct <309506350+lakeviewct@users.noreply.github.com>
2026-08-08 01:32:53 +0530

    finished FindRobloxBase()

--- PolarExternal/memory/find_roblox/find_roblox.cpp
+++ PolarExternal/memory/find_roblox/find_roblox.cpp
@@ -44,7 +44,45 @@ DWORD FindRobloxPID()
 	return FindRobloxProc().th32ProcessID;
 }
 
-uint64_t FindRoblox_Base()
+uint64_t FindRobloxBase()
 {
-	// to be done
+	// ----------
+
+	PROCESSENTRY32W roblox_process;
+	DWORD roblox_pid;
+	uint64_t roblox_base_address{};
+
+	HANDLE module_snapshot;
+	MODULEENTRY32W module_entry{};
+
+	bool search;
+
+	// ----------
+
+	roblox_process = FindRobloxProc();
+	roblox_pid = roblox_process.th32ProcessID;
+
+	module_snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, roblox_pid);
+	module_entry.dwSize = sizeof(module_entry);
+
+	// ----------
+
+	search = Module32FirstW(module_snapshot, &module_entry);
+
+	while (search)
+	{
+		if (wcsicmp(module_entry.szModule, L"RobloxPlayerBeta.exe") == false)
+		{
+			roblox_base_address = reinterpret_cast<uint64_t>(module_entry.modBaseAddr);
+			CloseHandle(module_snapshot);
+			return roblox_base_address;
+		}
+
+		search = Module32NextW(module_snapshot, &module_entry);
+	}
+
+	// ----------
+
+	CloseHandle(module_snapshot);
+	return roblox_base_address;
 }
