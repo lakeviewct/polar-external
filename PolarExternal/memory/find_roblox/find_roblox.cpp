0accd3da8235d351ec2bea4617142bea773c5f65
TheBigCIZ <TheBigCIZ@DESKTOP-CIZ>
2026-08-07 21:49:56 +0200

    fixed shit

--- PolarExternal/memory/find_roblox/find_roblox.cpp
+++ PolarExternal/memory/find_roblox/find_roblox.cpp
@@ -7,31 +7,30 @@ PROCESSENTRY32W FindRobloxProc()
 	HANDLE snapshot;
 
 	PROCESSENTRY32W current_process{};
-	bool search;
 	
 	// -----------
 
 	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-		if (snapshot == INVALID_HANDLE_VALUE)
-		{
-			return {};
-		}
+	if (snapshot == INVALID_HANDLE_VALUE)
+	{
+		return {};
+	}
 
 	current_process.dwSize = sizeof(current_process);
-	search = Process32FirstW(snapshot, &current_process);
 
 	// -----------
 
-	while (search)
+	if (Process32FirstW(snapshot, &current_process));
 	{
-		// compare name of current process with L"RobloxPlayerBeta.exe"
-		if (_wcsicmp(current_process.szExeFile, L"RobloxPlayerBeta.exe") == false)
+		do
 		{
-			CloseHandle(snapshot);
-			return current_process;
-		}
-
-		search = Process32NextW(snapshot, &current_process);
+			// compare name of current process with L"RobloxPlayerBeta.exe"
+			if (_wcsicmp(current_process.szExeFile, L"RobloxPlayerBeta.exe") == false)
+			{
+				CloseHandle(snapshot);
+				return current_process;
+			}
+		} while (Process32NextW(snapshot, &current_process));
 	}
 
 	// -----------
@@ -42,42 +41,7 @@ PROCESSENTRY32W FindRobloxProc()
 
 DWORD FindRobloxPID()
 {
-	// -----------
-
-	HANDLE snapshot;
-
-	PROCESSENTRY32W current_process{};
-	bool search;
-
-	// -----------
-
-	snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-	if (snapshot == INVALID_HANDLE_VALUE)
-	{
-		return 0;
-	}
-
-	current_process.dwSize = sizeof(current_process);
-	search = Process32FirstW(snapshot, &current_process);
-
-	// -----------
-
-	while (search)
-	{
-		// compare name of current process with L"RobloxPlayerBeta.exe"
-		if (_wcsicmp(current_process.szExeFile, L"RobloxPlayerBeta.exe") == false)
-		{
-			CloseHandle(snapshot);
-			return current_process.th32ProcessID;
-		}
-
-		search = Process32NextW(snapshot, &current_process);
-	}
-
-	// -----------
-
-	CloseHandle(snapshot);
-	return current_process.th32ProcessID;
+	return FindRobloxProc().th32ProcessID;
 }
 
 uint64_t FindRoblox_Base()
