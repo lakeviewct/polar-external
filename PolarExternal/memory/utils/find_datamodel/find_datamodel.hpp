55d4b64c4be96588d82729a6b928b6074b788fba
lakeviewct <309506350+lakeviewct@users.noreply.github.com>
2026-08-09 14:51:01 +0530

    wrote FindDatamodel(), added offsets.hpp

---
+++ PolarExternal/memory/utils/find_datamodel/find_datamodel.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+#include <Windows.h>
+
+#include "../../../misc_headers/offsets.hpp"
+
+	// find_datamodel.cpp
+uint64_t FindFakeDatamodel();
+uint64_t FindDatamodel();
--- PolarExternal/memory/utils/open_process/open_process.cpp
+++ PolarExternal/memory/utils/open_process/open_process.cpp
@@ -1,6 +1,7 @@
 #include "open_process.hpp"
 #include "../find_roblox/find_roblox.hpp"
 
+// Slower since you internally call FindRobloxPID()
 HANDLE OpenRobloxProcess()
 {
 	HANDLE roblox_handle;
@@ -11,6 +12,7 @@ HANDLE OpenRobloxProcess()
 	return roblox_handle;
 }
 
+// Use this one if you already have the pid:
 HANDLE OpenRobloxProcessPID(DWORD roblox_pid)
 {
 	HANDLE roblox_handle = OpenProcess(PROCESS_ALL_ACCESS, false, roblox_pid);
